866f37b719c6438b4563d13259ee584291451433
wikla pandu <wiklapandu2503@gmail.com>
2022-11-05 21:34:23 +0700

    feat(api): migration databases

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        //
-        Schema::create('shorturls', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('user_id');
-            $table->string('url');
-            $table->string('slug');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,7 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
-        Schema::drop('shorturls');
+        Schema::dropIfExists('users');
     }
-};
+}
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        //
-        Schema::create('shorturls', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('user_id');
-            $table->string('url');
-            $table->string('slug');
-            $table->timestamps();
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -30,7 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
-        Schema::drop('shorturls');
+        Schema::dropIfExists('password_resets');
     }
-};
+}
